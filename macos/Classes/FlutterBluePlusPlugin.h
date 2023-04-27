86545102f4abc5dc33f58e48dfac5800c55e0469
Chip Weinberger <weinberger.c@gmail.com>
2023-04-27 13:35:57 -0700

    [MacOS] copy iOS code to macOS, in preparation for macOS support

---
+++ macos/Classes/FlutterBluePlusPlugin.h
@@ -0,0 +1,16 @@
+// Copyright 2017, Paul DeMarco.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#import <Flutter/Flutter.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+#define NAMESPACE @"flutter_blue_plus"
+
+@interface FlutterBluePlusPlugin : NSObject<FlutterPlugin, CBCentralManagerDelegate, CBPeripheralDelegate>
+@end
+
+@interface FlutterBluePlusStreamHandler : NSObject<FlutterStreamHandler>
+@property FlutterEventSink sink;
+@property FlutterStandardTypedData *cachedBluetoothState;
+@end
