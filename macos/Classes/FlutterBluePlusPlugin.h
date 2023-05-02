9e24b67b8f104cd2931093485987d0afc39d80b2
John Whittington <git@jbrengineering.co.uk>
2023-05-02 14:34:07 +0200

    bare minimum macOS support

--- macos/Classes/FlutterBluePlusPlugin.h
+++ macos/Classes/FlutterBluePlusPlugin.h
@@ -2,7 +2,11 @@
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#if TARGET_OS_OSX
+#import <FlutterMacOS/FlutterMacOS.h>
+#else
 #import <Flutter/Flutter.h>
+#endif
 #import <CoreBluetooth/CoreBluetooth.h>
 
 #define NAMESPACE @"flutter_blue_plus"
