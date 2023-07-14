98caf63968d027ea44acb2459aac9bea1b62cc58
Chip Weinberger <weinberger.c@gmail.com>
2023-07-14 16:10:08 -0700

    Release 1.7

--- test/guid_test.dart
+++ test/guid_test.dart
@@ -2,6 +2,7 @@
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/*
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -60,3 +61,4 @@ main() {
     });
   });
 }
+*/
