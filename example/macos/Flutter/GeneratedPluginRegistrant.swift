7c38977f9ac6c0e0b3e760043b2533eb1bea50d7
Chip Weinberger <weinberger.c@gmail.com>
2023-07-22 13:22:45 -0700

    [Example] add macos support

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_blue_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+}
