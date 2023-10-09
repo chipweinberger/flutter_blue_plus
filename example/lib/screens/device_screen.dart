b310c8aafc03670bee4d26afcc1867d7613593f0
Chip Weinberger <weinberger.c@gmail.com>
2023-10-09 13:27:26 -0700

    [Example] fix missing import

--- example/lib/screens/device_screen.dart
+++ example/lib/screens/device_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 import '../widgets/service_tile.dart';
+import '../widgets/characteristic_tile.dart';
+import '../widgets/descriptor_tile.dart';
 import '../utils/snackbar.dart';
 import '../utils/extra.dart';
 
