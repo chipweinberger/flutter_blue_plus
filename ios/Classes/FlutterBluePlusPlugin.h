6a89bd79729760ece858ee9bdfe42c0c131be9f4
Chip Weinberger <weinberger.c@gmail.com>
2023-10-17 17:01:22 -0700

    [Copyright] update years & add Charles Weinberger

--- ios/Classes/FlutterBluePlusPlugin.h
+++ ios/Classes/FlutterBluePlusPlugin.h
@@ -1,4 +1,4 @@
-// Copyright 2023, Charles Weinberger & Paul DeMarco.
+// Copyright 2017-2023, Charles Weinberger & Paul DeMarco.
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
