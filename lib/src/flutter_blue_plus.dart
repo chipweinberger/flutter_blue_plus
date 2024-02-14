6437c723d5f098ccc9b2649c11e219dec100e413
Chip Weinberger <weinberger.c@gmail.com>
2024-02-14 14:50:13 -0800

    [Cleanup] small cleanup

--- lib/src/flutter_blue_plus.dart
+++ lib/src/flutter_blue_plus.dart
@@ -437,26 +437,22 @@ class FlutterBluePlus {
       var r = BmConnectionStateResponse.fromMap(call.arguments);
       _connectionStates[r.remoteId] = r;
       if (r.connectionState == BmConnectionStateEnum.disconnected) {
-        // reset known mtu
+        // clear mtu
         _mtuValues.remove(r.remoteId);
 
-        // clear lastDescs so that 'isNotifying' is reset
-        _lastDescs.remove(r.remoteId);
+        // clear lastDescs (resets 'isNotifying')
+        _lastDescs.remove(r.remoteId); 
 
-        // clear chr values, for api consistency
-        _lastChrs.remove(r.remoteId);
+        // clear lastChrs (api consistency)
+        _lastChrs.remove(r.remoteId); 
 
-        // Note: to make FBP easier to use, we purposely
-        // do not clear `knownServices` or `bondState`.
-        // to make FBP easier to use, we purposely do not clear knownServices,
-        // otherwise `servicesList` would be annoying to use.
-        // We also don't clear the `bondState` cache for faster performance.
+        // cancel & delete subscriptions
+        _deviceSubscriptions[r.remoteId]?.forEach((s) => s.cancel()); 
+        _deviceSubscriptions.remove(r.remoteId); 
 
-        _deviceSubscriptions[r.remoteId]?.forEach((s) => s.cancel()); // cancel subscriptions
-        _deviceSubscriptions.remove(r.remoteId); // delete subscriptions
-        _mtuValues.remove(r.remoteId); // reset known mtu
-        _lastDescs.remove(r.remoteId); // clear lastDescs so that 'isNotifying' is reset
-        _lastChrs.remove(r.remoteId); // for api consistency, clear characteristic values
+        // Note: to make FBP easier to use, we do not clear `knownServices`,
+        // otherwise `servicesList` would be more annoying to use. We also 
+        // do not clear `bondState`, for faster performance.
 
         // autoconnect
         if (_adapterStateNow == BmAdapterStateEnum.on) {
