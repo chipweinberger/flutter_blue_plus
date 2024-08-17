4870ffe5dc9b2882288c404a042b0e7885f04b4c
Thomas Clark <tnc1997@outlook.com>
2024-08-17 09:18:31 +0100

    refactor: create platform interface package

---
+++ packages/flutter_blue_plus_platform_interface/lib/flutter_blue_plus_platform_interface.dart
@@ -0,0 +1,1 @@
+export 'src/flutter_blue_plus_platform.dart';
---
+++ packages/flutter_blue_plus_platform_interface/lib/src/method_channel_flutter_blue_plus.dart
@@ -0,0 +1,4 @@
+import 'flutter_blue_plus_platform.dart';
+
+/// An implementation of [FlutterBluePlusPlatform] that uses method channels.
+class MethodChannelFlutterBluePlus extends FlutterBluePlusPlatform {}
