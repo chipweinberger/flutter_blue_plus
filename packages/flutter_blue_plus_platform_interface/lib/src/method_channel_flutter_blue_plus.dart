04ebb62e3ede073da9ffc48a8e3bbc7f48bed51e
Thomas Clark <tnc1997@outlook.com>
2024-08-21 06:46:53 +0100

    Revert "refactor: create platform interface package"
    This reverts commit 4870ffe5dc9b2882288c404a042b0e7885f04b4c.

--- packages/flutter_blue_plus_platform_interface/lib/src/method_channel_flutter_blue_plus.dart
+++
@@ -1,4 +0,0 @@
-import 'flutter_blue_plus_platform.dart';
-
-/// An implementation of [FlutterBluePlusPlatform] that uses method channels.
-class MethodChannelFlutterBluePlus extends FlutterBluePlusPlatform {}
