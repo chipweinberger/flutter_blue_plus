a47d35ae80b254315e78923a313561c0482f2e36
Chip Weinberger <weinberger.c@gmail.com>
2025-03-13 04:19:53 -0700

    Update packages/flutter_blue_plus_platform_interface/lib/flutter_blue_plus_platform_interface.dart
    Co-authored-by: Thomas Clark <tnc1997@outlook.com>

--- packages/flutter_blue_plus_platform_interface/lib/flutter_blue_plus_platform_interface.dart
+++ packages/flutter_blue_plus_platform_interface/lib/flutter_blue_plus_platform_interface.dart
@@ -226,8 +226,8 @@ abstract base class FlutterBluePlusPlatform {
     return Future.value(false);
   }
 
-  // Returns true if callers need to wait for Client Characteristic Configuration Descriptor to be written before returning.
-  // Throws an exception on error.
+  /// Returns true if callers need to wait for the Client Characteristic Configuration Descriptor (CCCD) to be written before returning.
+  /// Throws an exception on error.
   Future<bool> setNotifyValue(
     BmSetNotifyValueRequest request,
   ) {
