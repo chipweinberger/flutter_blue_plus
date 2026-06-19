5d41b7e98caa0a8cb9225a194ef7c8fc9cdf39e9
Chip Weinberger <weinberger.c@gmail.com>
2026-06-18 19:46:37 -0700

    Android: rename com.lib.flutter_blue_plus to com.jmx.flutter_blue_plus

--- packages/flutter_blue_plus/example/android/app/src/main/java/com/jmx/flutter_blue_plus_example/MainActivity.java
+++ packages/flutter_blue_plus/example/android/app/src/main/java/com/jmx/flutter_blue_plus_example/MainActivity.java
@@ -1,4 +1,4 @@
-package com.lib.flutter_blue_plus_example;
+package com.jmx.flutter_blue_plus_example;
 
 import io.flutter.embedding.android.FlutterActivity;
 
--- packages/flutter_blue_plus_android/android/src/main/java/com/jmx/flutter_blue_plus/FlutterBluePlusPlugin.java
+++ packages/flutter_blue_plus_android/android/src/main/java/com/jmx/flutter_blue_plus/FlutterBluePlusPlugin.java
@@ -2,7 +2,7 @@
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-package com.lib.flutter_blue_plus;
+package com.jmx.flutter_blue_plus;
 
 import android.Manifest;
 import android.annotation.TargetApi;
